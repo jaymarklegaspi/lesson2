9791a9a0b941a0ad15cb628c95e6e1ab989d3b5d
WS27 <johnlegaspi754@gmail.com>
2024-09-28 11:18:22 +0800

    my task

---
+++ task1haynako.py
@@ -0,0 +1,18 @@
+#print negative, print positive
+Age = [-12,13,-14,-15,-16]
+
+username = ["bodoy","balodoy","bitoy","pipitoy"]
+
+for dataage in age:
+    if dataAge < 0:
+         print(str(dataAge) +"negative")
+        elif dataAge > 0:
+            print(str(dataAge) + "positive")
+            
+        for datauser is username:
+            if datauser == "james"
+              print("james cute")
+            else:
+                print("james none")
+
+
